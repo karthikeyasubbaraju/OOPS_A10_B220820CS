0bd7607ff04fbf9008b86033ad55b9024586230f
karthikeyasubbaraju <156435590+karthikeyasubbaraju@users.noreply.github.com>
2024-10-26 10:30:41 +0530

    Update TextBook.java

--- DOP/src/oldshelf/TextBook.java
+++ DOP/src/oldshelf/TextBook.java
@@ -1,23 +1,43 @@
 package oldshelf;
 
+
 public class TextBook extends Book {
 
-	// TODO: Make this a final field with most strict visiibility possible.
-	String subject;
+	// DONE: Make this a final field with most strict visiibility possible.
+	private String subject;
 	
-	// TODO: Change the constructor if required? Correct any errors
+	// DONE: Change the constructor if required? Correct any errors
 	public TextBook(String subject) {
-		super(subject);
+		this.subject = subject;
 	}
 
-	// TODO : create a getter if required.
-	
-	// TODO: Create a setter if required
-	
-	// TODO: write a toString method
-	@Override
-	public String toString() {
-		return null;
+	// DONE : create a getter if required.
+	public String subject() {
+		return subject;
 	}
+
+	@Override
+	public final int hashCode(){
+		int m = 165;
+		int c = 30;
+		return m*subject.length() + c;
+	}
+
+	@Override
+	public final boolean equals(Object o){
+		if( o instanceof @SuppressWarnings("unused") TextBook t ){
+			return this.hashCode() == o.hashCode();
+		}
+		return false;
+	}
+	
+	
+	
+	// DONE: write a toString method
+	@Override
+	public final String toString() {
+		return "Subject : " + this.subject;
+	}
+
 
 }
